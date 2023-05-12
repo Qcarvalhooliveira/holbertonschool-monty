c5a5f6622e74d36b3f9b4bec9b5ae7a5aa4fec6f
root <queisecarvalho@hotmail.com>
2023-05-12 10:09:23 -0700

    main 1908

--- main.c
+++ main.c
@@ -1,18 +1,20 @@
 #include "monty.h"
 
 /**
- *lines_check - check line to arguments
- *@buffer: contain all
- *@line_number: count to lines inside the file
- *Return: Tokens
- */
-
+ *  * lines_check - check line to arguments
+ *   * @buffer: contain all
+ *    * @line_number: count to lines inside the file
+ *     * Return: Tokens
+ *      */
 char *lines_check(char *buffer, unsigned int line_number)
 {
 	char *token = NULL, *num_t = NULL;
 	long int i = 0;
 
 	token = strtok(buffer, " \t\n");
+	if (token == NULL)
+		return (NULL);
+
 	if (strcmp(token, "push") == 0)
 	{
 		num_t = strtok(NULL, " \t\n");
@@ -25,8 +27,8 @@ char *lines_check(char *buffer, unsigned int line_number)
 		}
 		for (i = 0; num_t[i] != '\0'; i++)
 		{
-			if (num_t[i] == '-')
-				i++;
+			if (num_t[i] == '-' && i == 0)
+				continue;
 			if (num_t[i] < 48 || num_t[i] > 57)
 			{
 				fprintf(stderr, "L%u: usage: push integer\n", line_number);
@@ -41,12 +43,11 @@ char *lines_check(char *buffer, unsigned int line_number)
 }
 
 /**
- *main - execute the monty interpreter
- *@argc: count number to arguments per line
- *@argv: array to arguments
- *Return: 0 if no exist or exit failure in case to error
- */
-
+ *  * main - execute the monty interpreter
+ *   * @argc: count number to arguments per line
+ *    * @argv: array to arguments
+ *     * Return: 0 if no exist or exit failure in case to error
+ *      */
 int main(int argc, char *argv[])
 {
 	stack_t *stack = NULL;
