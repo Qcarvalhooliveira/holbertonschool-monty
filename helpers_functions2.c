a8927c972998986e64064e35711758d7ebc3840c
root <queisecarvalho@hotmail.com>
2023-05-12 09:52:18 -0700

    helpers_functions2

--- helpers_functions2.c
+++ helpers_functions2.c
@@ -1,8 +1,16 @@
 #include "monty.h"
 
+/**
+ * is_numeric - checks if a string represents a numeric value
+ * @str: pointer to the string to be checked
+ *
+ * Return: 1 if the string is numeric, 0 otherwise.
+ */
+
 int is_numeric(char *str)
 {
 	int i = 0;
+
 	if (str == NULL)
 		return (0);
 	if (str[0] == '-' || str[0] == '+')
