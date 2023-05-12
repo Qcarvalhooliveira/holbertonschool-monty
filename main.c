fe1b3ba35ac070c9b094c4bf9b6742b0eb26e05f
root <queisecarvalho@hotmail.com>
2023-05-12 10:46:03 -0700

    main

--- main.c
+++ main.c
@@ -1,41 +1,30 @@
 #include "monty.h"
 
+int error_flag = 0;
+
 /**
- *  * lines_check - check line to arguments
- *   * @buffer: contain all
- *    * @line_number: count to lines inside the file
- *     * Return: Tokens
- *      */
+ *lines_check - check line to arguments
+ *@buffer: contain all
+ *@line_number: count to lines inside the file
+ *Return: Tokens
+ */
+
 char *lines_check(char *buffer, unsigned int line_number)
 {
 	char *token = NULL, *num_t = NULL;
-	long int i = 0;
 
 	token = strtok(buffer, " \t\n");
 	if (token == NULL)
 		return (NULL);
-
 	if (strcmp(token, "push") == 0)
 	{
 		num_t = strtok(NULL, " \t\n");
-		if (num_t == NULL)
+		if (num_t == NULL || !is_numeric(num_t))
 		{
 			fprintf(stderr, "L%u: usage: push integer\n", line_number);
 			free(buffer);
 			var_glob[1] = 1;
 			return (NULL);
-		}
-		for (i = 0; num_t[i] != '\0'; i++)
-		{
-			if (num_t[i] == '-' && i == 0)
-				continue;
-			if (num_t[i] < 48 || num_t[i] > 57)
-			{
-				fprintf(stderr, "L%u: usage: push integer\n", line_number);
-				free(buffer);
-				var_glob[1] = 1;
-				return (NULL);
-			}
 		}
 		var_glob[0] = atoi(num_t);
 	}
@@ -43,11 +32,12 @@ char *lines_check(char *buffer, unsigned int line_number)
 }
 
 /**
- *  * main - execute the monty interpreter
- *   * @argc: count number to arguments per line
- *    * @argv: array to arguments
- *     * Return: 0 if no exist or exit failure in case to error
- *      */
+ *main - execute the monty interpreter
+ *@argc: count number to arguments per line
+ *@argv: array to arguments
+ *Return: 0 if no exist or exit failure in case to error
+ */
+
 int main(int argc, char *argv[])
 {
 	stack_t *stack = NULL;
